5241804074ef359410aa739d76b6bbf9c44aa6a5
kevinMGII <100495893@alumnos.uc3m.es>
2024-06-11 01:53:22 +0200

    [Basic Events]

--- Bot/main.py
+++ Bot/main.py
@@ -1,7 +1,10 @@
 import discord
 from discord.ext import commands
 
-client = commands.Bot(command_prefix = ".", intents = discord.Intents.all())
+intents = discord.Intents.default()
+intents.members = True
+
+client = commands.Bot(command_prefix = ".", intents = intents)
 
 @client.event
 async def on_ready():
@@ -11,5 +14,28 @@ async def on_ready():
 async def hello(ctx):
     await ctx.send("[Hi]")
 
-client.run("MTI0OTg1MTcwNDA1NzUzMjQ5OA.Ge2BU4.g2JH2kvturV8667JnKXsBWKyZZzhjieEXmgrms")
+@client.command()
+async def bye(ctx):
+    await ctx.send("[Bye]")
 
+@client.event
+async def on_member_join(member):
+    channel = client.get_channel(1249870241140441182)
+    await channel.send("[Hi " + member + " ]")
+
+@client.event
+async def on_member_remove(member):
+    channel = client.get_channel(1249870241140441182)
+    await channel.send("[Bye " + member + " ]")
+
+@client.event
+async def on_member_ban(guild, user):
+    channel = client.get_channel(1249871058152980530)
+    await channel.send("[User " + user + " banned from " + guild + " ]")
+
+@client.event
+async def on_member_unban(guild, user):
+    channel = client.get_channel(1249871058152980530)
+    await channel.send("[User " + user + " banned from " + guild + " ]")
+
+client.run("MTI0OTg1MTcwNDA1NzUzMjQ5OA.Ge2BU4.g2JH2kvturV8667JnKXsBWKyZZzhjieEXmgrms")
